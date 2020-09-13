aefe1f1508d8ad4bb4ab97b5fcae4a1036eeea56
yen-tingLin <aillvone@gmail.com>
2020-09-13 14:53:33 +0800

    core part of user authentication for login

--- src/main/java/com/example/post/config/SecurityConfig.java
+++ src/main/java/com/example/post/config/SecurityConfig.java
@@ -1,32 +1,44 @@
 package com.example.post.config;
 
+import com.example.post.exception.SpringPostException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final UserDetailsService userDetailsService;
+
+    @Autowired
+    SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        // csrf protetion mainly occurs when there are sessions or 
+        // csrf protetion mainly occurs when there are sessions or
         // we are using cookies to authenticate session information
         httpSecurity.csrf().disable()
-                    // allow all the incoming requests for backend api
-                    // whose end point start with /api/auth
-                    .authorizeRequests()
-                    .antMatchers("/api/auth/**")
-                    .permitAll()
-                    .anyRequest()
-                    .authenticated();
+                // allow all the incoming requests for backend api
+                // whose end point start with /api/auth
+                .authorizeRequests()
+                .antMatchers("/api/auth/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
 
     }
 
     // algo : Bcrypt hashing.
-    // as PasswordEncoder is an interface, 
+    // as PasswordEncoder is an interface,
     // we have to manually create a bean inside configuretion class,
     // whenever we are autowire this bean,
     // we will get an instance of type BCryptPasswordEncoder
@@ -34,5 +46,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    // create authentication manager by authentication manager builder
+    // inject AuthenticationManagerBuilder object by method injection
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) {
+        // take input of type UserDetailsService
+        // UserDetailsService is an interface
+        try {
+            authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+        
+        } catch (Exception e) {
+            throw new SpringPostException("Failed to create authentication manager", e);
+        }
+    } 
     
 }
---
+++ src/main/java/com/example/post/service/UserDetailsServiceImp.java
@@ -0,0 +1,45 @@
+package com.example.post.service;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
+
+import com.example.post.exception.SpringPostException;
+import com.example.post.model.User;
+import com.example.post.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImp implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    UserDetailsServiceImp(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> optionalUser = userRepository.findByUserName(username);
+        User userFound = optionalUser.orElseThrow(() -> new SpringPostException("User not found with " + username));
+
+        return new org.springframework.security.core.userdetails.User(
+                userFound.getUserName(), userFound.getPassword(), userFound.isValidated(), 
+                true, true, true, getAuthorities("USER"));
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(String role) {
+        return Collections.singletonList(new SimpleGrantedAuthority(role));
+    }
+
+    
+    
+}

047ccc69a2654e0f3536d12205ba67a5e2b83095
yen-tingLin <aillvone@gmail.com>
2020-09-19 13:23:00 +0800

    document REST API using Swagger & Springfox

--- src/main/java/com/example/post/PostApplication.java
+++ src/main/java/com/example/post/PostApplication.java
@@ -1,11 +1,15 @@
 package com.example.post;
 
+import com.example.post.config.SwaggerConfiguration;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@Import(SwaggerConfiguration.class)
 public class PostApplication {
 
 	public static void main(String[] args) {
